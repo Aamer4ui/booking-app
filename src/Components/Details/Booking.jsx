633c6cc94ac5950c0f3613031d28be3ceabc8b9a
Aamer4ui <aamersohelansari@gmail.com>
2021-01-29 12:48:37 +0530

    booking form

--- src/Components/Details/Booking.jsx
+++ src/Components/Details/Booking.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import "../Details/Style.css";
+import { Form, Row, Col, Button } from "react-bootstrap";
 export const Booking = () => {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>Online Booking</h1>
 
-      <Form>
+      <Form style={{ textAlign: "center" }}>
         <Form.Group as={Row} controlId="formHorizontalEmail">
           <Form.Label column sm={2}>
             Email
@@ -21,38 +22,6 @@ export const Booking = () => {
           </Form.Label>
           <Col sm={10}>
             <Form.Control type="password" placeholder="Password" />
-          </Col>
-        </Form.Group>
-        <fieldset>
-          <Form.Group as={Row}>
-            <Form.Label as="legend" column sm={2}>
-              Radios
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Check
-                type="radio"
-                label="first radio"
-                name="formHorizontalRadios"
-                id="formHorizontalRadios1"
-              />
-              <Form.Check
-                type="radio"
-                label="second radio"
-                name="formHorizontalRadios"
-                id="formHorizontalRadios2"
-              />
-              <Form.Check
-                type="radio"
-                label="third radio"
-                name="formHorizontalRadios"
-                id="formHorizontalRadios3"
-              />
-            </Col>
-          </Form.Group>
-        </fieldset>
-        <Form.Group as={Row} controlId="formHorizontalCheck">
-          <Col sm={{ span: 10, offset: 2 }}>
-            <Form.Check label="Remember me" />
           </Col>
         </Form.Group>
 
